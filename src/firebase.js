7393975615839046b7d71196a9e0b2edd1221a10
Maroof Khan <maroofk2108@gmail.com>
2026-04-13 01:22:03 +0530

    Update Firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // Replace these with your own project's configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+  apiKey: "AIzaSyA6FCzGWZiwJjwLnWZXR4CfD4HvUTM9udw",
+  authDomain: "academics-tracker-7f776.firebaseapp.com",
+  projectId: "academics-tracker-7f776",
+  storageBucket: "academics-tracker-7f776.firebasestorage.app",
+  messagingSenderId: "832163143871",
+  appId: "1:832163143871:web:3ea71fae7566ae82aea60a",
+  measurementId: "G-YB6DC06VWK"};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
